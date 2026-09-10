a203d10df007aecbfca115641074b3fac6745272
Willy Vox <huyfrontend@hotmail.com>
2026-09-11 00:44:19 +1000

    fix build error

--- src/app/crypto/exchanges/page.tsx
+++ src/app/crypto/exchanges/page.tsx
@@ -10,6 +10,8 @@ export const metadata = buildMetadata({
   path: "/crypto/exchanges",
 });
 
+export const dynamic = "force-dynamic";
+
 export default async function ExchangesPage() {
   const { items } = await getProviders();
   return (
